75a52f1240a23f00804f8f96575e809319deccd0
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-10-22 08:22:18 +0200

    Removed useless import.

--- csskp/wsgi.py
+++ csskp/wsgi.py
@@ -8,10 +8,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-
-sys.path.insert(0, '/home/cases/Fit4Cybersecurity/')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csskp.settings')
 
