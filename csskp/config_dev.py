a2626e261cb9cc81688600c80bc3df43b4bdd7b9
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-10-23 08:18:37 +0200

    Add the possibility to custom BOOTSTRAP4 configuration variable via the config module.

--- csskp/config_dev.py
+++ csskp/config_dev.py
@@ -10,3 +10,79 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+BOOTSTRAP4 = {
+
+    # The complete URL to the Bootstrap CSS file
+    # Note that a URL can be either a string,
+    # e.g. "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+    # or a dict like the default value below.
+    "css_url": {
+        "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+        "crossorigin": "anonymous",
+    },
+
+    # The complete URL to the Bootstrap JavaScript file
+    "javascript_url": {
+        "url": "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+        "crossorigin": "anonymous",
+    },
+
+    # The complete URL to the Bootstrap CSS file (None means no theme)
+    "theme_url": None,
+
+    # The URL to the jQuery JavaScript file (full)
+    "jquery_url": {
+        "url": "https://code.jquery.com/jquery-3.4.1.min.js",
+        "crossorigin": "anonymous",
+    },
+
+    # The URL to the jQuery JavaScript file (slim)
+    "jquery_slim_url": {
+        "url": "https://code.jquery.com/jquery-3.4.1.slim.min.js",
+        "crossorigin": "anonymous",
+    },
+
+    # The URL to the Popper.js JavaScript file (slim)
+    "popper_url": {
+        "url": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+        "crossorigin": "anonymous",
+    },
+
+    # Put JavaScript in the HEAD section of the HTML document (only relevant if you use bootstrap4.html)
+    'javascript_in_head': True,
+
+    # Include jQuery with Bootstrap JavaScript False|falsy|slim|full (default=False)
+    # False - means tag bootstrap_javascript use default value - `falsy` and does not include jQuery)
+    'include_jquery': True,
+
+    # Label class to use in horizontal forms
+    'horizontal_label_class': 'col-md-3',
+
+    # Field class to use in horizontal forms
+    'horizontal_field_class': 'col-md-9',
+
+    # Set placeholder attributes to label if no placeholder is provided
+    'set_placeholder': True,
+
+    # Class to indicate required (better to set this in your Django form)
+    'required_css_class': '',
+
+    # Class to indicate error (better to set this in your Django form)
+    'error_css_class': 'is-invalid',
+
+    # Class to indicate success, meaning the field has valid input (better to set this in your Django form)
+    'success_css_class': 'is-valid',
+
+    # Renderers (only set these if you have studied the source and understand the inner workings)
+    'formset_renderers':{
+        'default': 'bootstrap4.renderers.FormsetRenderer',
+    },
+    'form_renderers': {
+        'default': 'bootstrap4.renderers.FormRenderer',
+    },
+    'field_renderers': {
+        'default': 'bootstrap4.renderers.FieldRenderer',
+        'inline': 'bootstrap4.renderers.InlineFieldRenderer',
+    },
+}
