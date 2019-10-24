fbe0e3d2802b0281a6995c45c398d8ec9c0a36ff
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-10-24 11:34:30 +0200

    fix

--- survey/context_processors.py
+++ survey/context_processors.py
@@ -2,10 +2,13 @@
 import json
 
 def get_version(request):
-    with open('VERSION.json', 'r') as json_file:
-        data = json.load(json_file)
-        version = "{}.{}.{}".format(data['major'], data['minor'],
-                                        data['hotfix'])
+    try:
+        with open('VERSION.json', 'r') as json_file:
+            data = json.load(json_file)
+            version = "{}.{}.{}".format(data['major'], data['minor'],
+                                            data['hotfix'])
+    except:
+        version = "0.0.1"
     return {
         'app_version': version
     }
