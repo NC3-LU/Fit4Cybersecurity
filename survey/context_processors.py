90f68e1335b2370b732b91bf13870e98f2ba518a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-10-24 11:39:48 +0200

    uses BASE_DIR to find the VERSION.json file

--- survey/context_processors.py
+++ survey/context_processors.py
@@ -1,9 +1,11 @@
 
+import os
 import json
+from csskp.settings import BASE_DIR
 
 def get_version(request):
     try:
-        with open('VERSION.json', 'r') as json_file:
+        with open(os.path.join(BASE_DIR,'VERSION.json'), 'r') as json_file:
             data = json.load(json_file)
             version = "{}.{}.{}".format(data['major'], data['minor'],
                                             data['hotfix'])
