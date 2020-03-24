69c7870823a4adda47a2803024033e6bbfe65498
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-03-25 00:17:05 +0100

    added Docker python:3.8-alpine image. wip

--- csskp/config_dev.py
+++ csskp/config_dev.py
@@ -11,6 +11,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
+        "PASSWORD": "password",
         "HOST": "db",
         "PORT": 5432,
     }
--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/ash
 # wait-for-postgres.sh
 
 set -e
 
 shift
 
-until (! command -v psql || PGPASSWORD=postgres psql -h db -U "postgres" -c '\q' )
+until (! command -v psql || PGPASSWORD=password psql -h db -U "postgres" -c '\q' )
 do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
@@ -13,6 +13,10 @@ done
 
 >&2 echo "Postgres is up - executing command"
 poetry run python manage.py migrate
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=password
+export DJANGO_SUPERUSER_EMAIL=admin@admin.localhost
+poetry run python manage.py createsuperuser --noinput 2>/dev/null
 # Can be used one of apache service or python server.
 #apache2ctl -D FOREGROUND
-poetry run python manage.py runserver 0.0.0.0:80
+poetry run python manage.py runserver 0.0.0.0:8000
