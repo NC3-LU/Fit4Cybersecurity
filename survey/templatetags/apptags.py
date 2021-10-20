cf814673bef36a5bf3e6e9f06ad852805087ad78
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-10-20 17:36:22 +0200

    chg: [style] reformat files.

--- survey/templatetags/apptags.py
+++ survey/templatetags/apptags.py
@@ -4,7 +4,7 @@ from django.utils import translation
 register = template.Library()
 
 
-@register.filter(name='translate')
+@register.filter(name="translate")
 def translate(string, language):
     with translation.override(language):
         return translation.gettext(string)
