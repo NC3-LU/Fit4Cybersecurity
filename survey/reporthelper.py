9dba7bfe8e7870dbb1d2340c474db1ef93afe1ad
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-11-08 21:24:50 +0100

    chg: [core] Removed now useless/dead code.

--- survey/reporthelper.py
+++ survey/reporthelper.py
@@ -5,11 +5,9 @@ import io
 import os
 import base64
 import logging
-from django.http import HttpResponse
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
-from csskp.settings import PICTURE_DIR, CUSTOM
+from csskp.settings import PICTURE_DIR
 from survey.models import (
     SurveyQuestion,
     SurveyQuestionAnswer,
@@ -21,42 +19,17 @@ from survey.models import (
 from utils.radarFactory import radar_factory
 import matplotlib.pyplot as plt
 
-# remove html tags from the report
-# https://stackoverflow.com/questions/753052/strip-html-from-strings-in-python
-from io import StringIO
-from html.parser import HTMLParser
-
-import docx
-from docx import Document
-from docx.shared import Cm, Pt
-
-# from docx.enum.text import WD_ALIGN_PARAGRAPH
-from datetime import date
-import re
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-class MLStripper(HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs = True
-        self.text = StringIO()
+def get_formatted_translations(lang: str, type: str) -> Dict:
+    translations = TranslationKey.objects.filter(lang=lang, ttype=type)
+    translation_key_values = {}
+    for translation in translations:
+        translation_key_values[translation.key] = translation.text
 
-    def handle_data(self, d):
-        self.text.write(d)
-
-    def get_data(self):
-        return self.text.getvalue()
-
-
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
+    return translation_key_values
 
 
 def getRecommendations(user: SurveyUser, lang: str):
@@ -96,7 +69,6 @@ def getRecommendations(user: SurveyUser, lang: str):
                 finalReportRecs[category_name].append(
                     recommendations_translations[rec.textKey]
                 )
-
     return finalReportRecs
 
 
@@ -107,236 +79,6 @@ def is_recommendation_already_added(recommendation: str, recommendations: dict):
                 return True
 
     return False
-
-
-def createAndSendReport(user: SurveyUser, lang: str):
-    """Generates the report as a .docx file, then returns it to the view."""
-
-    filepath = os.path.join(settings.BASE_DIR, settings.REPORT_TEMPLATE_DIR)
-
-    template = os.path.join(filepath, "template-" + lang + ".docx")
-    doc = Document(template)
-
-    introduction = ""
-    file_path = os.path.join(filepath, lang + "_intro.txt")
-    try:
-        with open(file_path, encoding="UTF-8") as f:
-            introduction = f.read()
-    except Exception as e:
-        logger.error("Error when reading template: {}".format(e))
-        raise Exception("Oooops... something went wrong!")
-
-    introduction = introduction.replace("\n\r", "\n")
-    x = 0
-    for i in introduction.split("\n\n"):
-        if x == 0:
-            doc.add_heading(i, level=1)
-            x += 1
-            continue
-        doc.add_paragraph(i)
-
-    methodDescr = ""
-    file_path = os.path.join(filepath, lang + "_description.txt")
-    try:
-        with open(file_path, encoding="UTF-8") as f:
-            methodDescr = f.read()
-    except Exception as e:
-        logger.error("Error when reading template: {}".format(e))
-        raise Exception("Oooops... something went wrong!")
-
-    methodDescr = methodDescr.replace("\n\r", "\n")
-    methodDescr = methodDescr.replace(
-        "$$qnumber$$", str(len(SurveyQuestion.objects.all()))
-    )
-    x = 0
-    for i in methodDescr.split("\n\n"):
-        if x == 0:
-            doc.add_heading(i, level=1)
-            x += 1
-            continue
-        doc.add_paragraph(i)
-
-    results = ""
-    file_path = os.path.join(filepath, lang + "_resultdisclaimer.txt")
-    try:
-        with open(file_path, encoding="UTF-8") as f:
-            results = f.read()
-    except Exception as e:
-        logger.error("Error when reading template: {}".format(e))
-        raise Exception("Oooops... something went wrong!")
-
-    score, details, section_list = calculateResult(user, lang)
-
-    results = results.replace("\n\r", "\n")
-    # results = results.replace("$$result$$", str(score))
-    x = 0
-    for i in results.split("\n\n"):
-        if x == 0:
-            doc.add_heading(i, level=1)
-            x += 1
-            continue
-
-        if "$$result$$" in i:
-            i_list = i.split("$$result$$")
-            p = doc.add_paragraph()
-            for idx, elem in enumerate(i_list):
-                p.add_run(elem)
-                # p.alignment = WD_ALIGN_PARAGRAPH.CENTER
-                if idx + 1 < len(i_list):
-                    px = p.add_run(str(score))
-                    px.font.bold = True
-        else:
-            doc.add_paragraph(i)
-
-    try:
-        chart_png_file = generate_chart_png(user, details, section_list, lang)
-        doc.add_paragraph()
-        paragraph = doc.add_paragraph()
-        run = paragraph.add_run()
-        run.add_picture(chart_png_file)
-    except Exception as e:
-        logger.error("{}".format(e))
-
-    doc.add_paragraph()
-
-    recommendationList = getRecommendations(user, lang)
-
-    doc.add_page_break()
-
-    recs = ""
-    file_path = os.path.join(filepath, lang + "_recs.txt")
-    try:
-        with open(file_path, encoding="UTF-8") as f:
-            recs = f.read()
-    except Exception as e:
-        logger.error("Error when reading template: {}".format(e))
-        raise Exception("Oooops... something went wrong!")
-
-    recs = recs.replace("\n\r", "\n")
-    x = 0
-    for i in recs.split("\n\n"):
-        if x == 0:
-            doc.add_heading(i, level=1)
-            x += 1
-            continue
-        doc.add_paragraph(i)
-
-    for category, items in recommendationList.items():
-        doc.add_heading(category, level=2)
-        point_number = 1
-        for recommendation in items:
-            # Create hyperlinks in the document.
-            split_links = re.split(
-                r'\<a href="(.+?)".*>([\w\s]+)\<\/a\>', recommendation
-            )
-            elements_number = len(split_links)
-            if elements_number > 1:
-                paragraph = doc.add_paragraph(
-                    str(point_number) + ". ", "List Paragraph"
-                )
-                for index, string_part in enumerate(split_links):
-                    if index % 3 == 0:
-                        paragraph.add_run(string_part)
-                        if elements_number > index + 1:
-                            add_hyperlink(
-                                paragraph,
-                                split_links[index + 2],
-                                split_links[index + 1],
-                            )
-            else:
-                doc.add_paragraph(
-                    str(point_number) + ". " + strip_tags(recommendation),
-                    "List Paragraph",
-                )
-            point_number += 1
-
-    doc.add_page_break()
-
-    doc.add_heading(_("Questions"), level=1)
-
-    questions_translations = get_formatted_translations(lang, "Q")
-    answers_translations = get_formatted_translations(lang, "A")
-
-    questions = SurveyQuestion.objects.all().order_by("qindex")
-
-    for index, question in enumerate(questions):
-        table = doc.add_table(rows=1, cols=2)
-        table.autofit = False
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = str(index + 1)
-        hdr_cells[1].text = strip_tags(questions_translations[question.titleKey])
-
-        bX = hdr_cells[0].paragraphs[0].runs[0]
-        bX.font.bold = True
-        bX.font.size = Pt(13)
-        bX = hdr_cells[1].paragraphs[0].runs[0]
-        bX.font.bold = True
-        bX.font.size = Pt(13)
-
-        answers_list = SurveyQuestionAnswer.objects.filter(question=question).order_by(
-            "aindex"
-        )
-        user_answers = SurveyUserAnswer.objects.filter(user=user)
-        user_answers_values = {}
-        for user_answer in user_answers:
-            user_answers_values[user_answer.answer_id] = user_answer
-
-        for answer in answers_list:
-            row_cells = table.add_row().cells
-
-            user_answer = user_answers_values[answer.id]
-            if user_answer.uvalue > 0:
-                row_cells[0].text = "X"
-                bX = row_cells[0].paragraphs[0].runs[0]
-                bX.font.bold = True
-            else:
-                row_cells[0].text = " "
-
-            row_cells[1].text = strip_tags(answers_translations[answer.answerKey])
-
-            if user_answer.uvalue > 0:
-                bX = row_cells[1].paragraphs[0].runs[0]
-                bX.font.bold = True
-                if user_answer.content != "":
-                    content_row_cells = table.add_row().cells
-                    content_row_cells[0].text = " "
-                    content_row_cells[1].text = strip_tags(user_answer.content)
-
-        col = table.columns[0]
-        col.width = Cm(1.0)
-        col = table.columns[1]
-        col.width = Cm(14.0)
-        for cell in table.columns[0].cells:
-            cell.width = Cm(1.0)
-        for cell in table.columns[1].cells:
-            cell.width = Cm(14.0)
-
-        doc.add_paragraph()
-
-    section = doc.sections[0]
-    header = section.header
-    paragraph = header.paragraphs[0]
-    paragraph.text = str(date.today()) + "\t\t" + CUSTOM["tool_name"]
-    paragraph.style = doc.styles["Header"]
-
-    response = HttpResponse(
-        content_type="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    )
-    response["Content-Disposition"] = (
-        "attachment; filename=Report_"
-        + CUSTOM["tool_name"]
-        + "_"
-        + str(date.today())
-        + "_"
-        + lang
-        + ".docx"
-    )
-    doc.save(response)
-
-    # make checkboxes to recommendation and a single button of get companies
-    # then call getcompanies when button is hit
-
-    return response
 
 
 def calculateResult(user: SurveyUser, lang: str):
@@ -449,45 +191,3 @@ def generate_chart_png(
         plt.close()
 
     return file_name
-
-
-def get_formatted_translations(lang: str, type: str) -> Dict:
-    translations = TranslationKey.objects.filter(lang=lang, ttype=type)
-    translation_key_values = {}
-    for translation in translations:
-        translation_key_values[translation.key] = translation.text
-
-    return translation_key_values
-
-
-def add_hyperlink(paragraph, text: str, url: str):
-    # This gets access to the document.xml.rels file and gets a new relation id value
-    part = paragraph.part
-    r_id = part.relate_to(
-        url, docx.opc.constants.RELATIONSHIP_TYPE.HYPERLINK, is_external=True
-    )
-
-    # Create the w:hyperlink tag and add needed values
-    hyperlink = docx.oxml.shared.OxmlElement("w:hyperlink")
-    hyperlink.set(
-        docx.oxml.shared.qn("r:id"),
-        r_id,
-    )
-
-    # Create a w:r element and a new w:rPr element
-    new_run = docx.oxml.shared.OxmlElement("w:r")
-    rPr = docx.oxml.shared.OxmlElement("w:rPr")
-
-    # Set link color and underline
-    c = docx.oxml.shared.OxmlElement("w:color")
-    c.set(docx.oxml.shared.qn("w:val"), "FF8822")
-    rPr.append(c)
-
-    # Join all the xml elements together add add the required text to the w:r element
-    new_run.append(rPr)
-    new_run.text = text
-    hyperlink.append(new_run)
-
-    paragraph._p.append(hyperlink)
-
-    return hyperlink
