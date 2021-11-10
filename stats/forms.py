35db6cff927d9345a3d52ea6901ca52c99e23a77
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-11-10 10:35:37 +0100

    chg: [template] added form-control CSS class to date picker.

--- stats/forms.py
+++ stats/forms.py
@@ -13,10 +13,14 @@ def one_month_before_today():
 
 class DatesLimitForm(forms.Form):
     start_date = forms.DateField(
-        widget=DatePickerInput(format=DEFAULT_DATE_FORMAT).start_of("event"),
+        widget=DatePickerInput(
+            format=DEFAULT_DATE_FORMAT, attrs={"class": "form-control"}
+        ).start_of("event"),
         initial=one_month_before_today(),
     )
     end_date = forms.DateField(
-        widget=DatePickerInput(format=DEFAULT_DATE_FORMAT).end_of("event"),
+        widget=DatePickerInput(
+            format=DEFAULT_DATE_FORMAT, attrs={"class": "form-control"}
+        ).end_of("event"),
         initial=datetime.today(),
     )
