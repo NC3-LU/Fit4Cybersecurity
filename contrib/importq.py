fba2971fc68b12227728356b655b01f25adda92d
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-11-16 09:06:49 +0100

    added script to create JSON file for populating an instance

---
+++ contrib/importq.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""
+Generate JSON files with the output of the command:
+
+$ python manage.py dumpdata --indent 2 survey > ./contrib/out.json
+"""
+
+import json
+
+with open("out.json") as f:
+    data = json.loads(f.read())
+
+sections = [elem for elem in data if elem["model"] == "survey.surveysection"]
+categories = [
+    elem for elem in data if elem["model"] == "survey.surveyquestionservicecategory"
+]
+questions = [elem for elem in data if elem["model"] == "survey.surveyquestion"]
+answers = [elem for elem in data if elem["model"] == "survey.surveyquestionanswer"]
+translations = [elem for elem in data if elem["model"] == "survey.translationkey"]
+recommendations = [elem for elem in data if elem["model"] == "survey.recommendations"]
+
+
+questions_json = []
+
+for question in questions:
+
+    new_question = question["fields"]
+
+    # Related answers
+    related_answers = [
+        answer for answer in answers if answer["fields"]["question"] == question["pk"]
+    ]
+    # Related category
+    related_category = [
+        category
+        for category in categories
+        if category["pk"] == question["fields"]["service_category"]
+    ][0]
+    # Related section
+    related_section = [
+        section
+        for section in sections
+        if section["pk"] == question["fields"]["section"]
+    ][0]
+
+    new_question["answers"] = []
+    for answer in related_answers:
+        # Related recommendations
+        related_recommendations = [
+            recommendation["fields"]
+            for recommendation in recommendations
+            if recommendation["fields"]["forAnswer"] == answer["pk"]
+        ]
+
+        print(related_recommendations)
+
+        new_question["answers"].append(
+            {
+                "answerKey": answer["fields"]["answerKey"],
+                "aindex": answer["fields"]["aindex"],
+                "uniqueAnswer": answer["fields"]["uniqueAnswer"],
+                "score": answer["fields"]["score"],
+                "atype": answer["fields"]["atype"],
+                "recommendations": related_recommendations
+            }
+        )
+
+    new_question["service_category"] = related_category["fields"]["titleKey"]
+    new_question["section"] = related_section["fields"]["sectionTitleKey"]
+
+    questions_json.append(new_question)
+
+
+
+with open("questions.json", "w") as f:
+    f.write(json.dumps(questions_json, indent=4, sort_keys=False))
+
+
+# print(questions_json)
