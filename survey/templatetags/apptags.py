b5a0037ab42b6d13ae3029f09391a4d0a27e8b20
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-12-01 18:08:36 +0100

    chg: [report] ensure the variable of replacement will be a instance of string.

--- survey/templatetags/apptags.py
+++ survey/templatetags/apptags.py
@@ -67,8 +67,8 @@ def find_tuple(list, key):
 
 
 @register.filter
-def strreplace(text, changes):
+def strreplace(text: str, changes: str) -> str:
     chg_list = [arg.strip() for arg in changes.split(',')]
     for from_, to in [(chg_list[0], chg_list[1])]:
-        text = text.replace(from_, to, -1)
+        text = text.replace(from_, str(to), -1)
     return text
