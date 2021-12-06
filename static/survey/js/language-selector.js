e22ec9b9148f6c09ef8f42fa56ecbea609648799
jfrocha <juanferocha@hotmail.com>
2021-12-06 15:33:49 +0100

    Added condition if languageSelector exists

--- static/survey/js/language-selector.js
+++ static/survey/js/language-selector.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-    var currentLanguageValue = $("#language-selector a.dropdown-toggle").html().replace(/\r?\n|\s/g, "");
-    $("#language-selector .dropdown-item").click(function(event) {
-        if ($(this).html().replace(/\r?\n|\s/g, "") == currentLanguageValue) {
-            event.preventDefault();
-        }
-    });
+    let languageSelector = $("#language-selector a.dropdown-toggle").html();
+    if (languageSelector) {
+        var currentLanguageValue = languageSelector.replace(/\r?\n|\s/g, "");
+        $("#language-selector .dropdown-item").click(function(event) {
+            if ($(this).html().replace(/\r?\n|\s/g, "") == currentLanguageValue) {
+                event.preventDefault();
+            }
+        });
+    }
 });
