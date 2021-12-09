e036a6dbfa70d571e46f90c18520e7ad3593be41
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2021-12-09 14:43:23 +0100

    Moved from variable as it may be not needed if a user object is returned.

--- survey/viewLogic.py
+++ survey/viewLogic.py
@@ -63,7 +63,6 @@ def handle_start_survey(request: HttpRequest, lang: str) -> Union[Dict, SurveyUs
     questions = SurveyQuestion.objects.filter(
         section__label__contains="__context"
     ).all()
-    questions_per_id = {question.id: question for question in questions}
 
     # if no context questions
     if not questions.count() and request.method == "GET":
@@ -136,7 +135,7 @@ def handle_start_survey(request: HttpRequest, lang: str) -> Union[Dict, SurveyUs
     return {
         "title": title,
         "forms": forms,
-        "questions_per_id": questions_per_id,
+        "questions_per_id": {question.id: question for question in questions},
         "action": action,
         "choosen_lang": lang,
     }
