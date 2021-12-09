1cbd33eb0a5fbf7a00fbe3d4cf9a77870f2fd90c
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-12-09 13:20:31 +0100

    chg: [style] reformat with black. !minor

--- survey/forms.py
+++ survey/forms.py
@@ -2,9 +2,7 @@
 
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from survey.globals import SECTOR_CHOICES, COMPANY_SIZE, COUNTRIES
 from survey.models import SurveyQuestionAnswer
-from django_countries.fields import CountryField
 import json
 
 
