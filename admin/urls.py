62da9ad74214eb42c44c2d5c05a6d872794230a3
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-12-15 11:45:20 +0100

    chg: [templates] added option to generate mo files from the admin page.

--- admin/urls.py
+++ admin/urls.py
@@ -6,4 +6,5 @@ from admin import views
 
 urlpatterns = [
     path("export/survey.json", views.export_survey_json),
+    path("compile-translations", views.compile_translations, name="compile_translations"),
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from django.http import JsonResponse
+import sys
+import subprocess
+from django.http import JsonResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from survey.lib.utils import export_survey
 
@@ -9,3 +11,14 @@ from survey.lib.utils import export_survey
 def export_survey_json(request):
     result = export_survey()
     return JsonResponse(result, safe=False)
+
+
+@login_required
+def compile_translations(request):
+    cmd = [
+        sys.exec_prefix + "/bin/python",
+        "manage.py",
+        "compilemessages",
+    ]
+    subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    return HttpResponseRedirect("/admin/")
