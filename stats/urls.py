08bfcf7f3f4f6040a0090f19bcdf8b2369f5536a
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-12-15 11:22:01 +0100

    chg: [templates] updated default index Django admin template in order to add links to different admin tools.

--- stats/urls.py
+++ stats/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path
-
 from stats import views
 
+app_name = 'stats'
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
 ]
