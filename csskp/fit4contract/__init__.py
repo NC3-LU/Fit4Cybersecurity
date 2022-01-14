9d2de47740c04182a08fa16fbbca29a50bd4d7be
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2022-01-14 16:46:09 +0100

    Added basic files for the site.

---
+++ csskp/fit4contract/__init__.py
@@ -0,0 +1,65 @@
+from django.utils.translation import gettext_lazy
+
+CUSTOM = {
+    # Generic configurations
+    "tool_name": "Fit4Contract",
+    "intro_text": gettext_lazy(
+        "This questionnaire aims to identify the general scope of the contractual "
+        + "relationship considered, in order to set up an acquisition, a lease, "
+        + "a development or any other service in the field of information and communication "
+        + "technologies. Based on this scope, Fit4Contract provides a list of basic "
+        + "information security requirements that are recommended to be considered in "
+        + "establishing the contractual relationship. This is a self assessment tool and "
+        + "as such cannot provide definitive guidelines. This tool is simply raising "
+        + "awareness for Information Security in ICT product and service contracts. "
+        + "Additionally, many countries have their own legal requirements for contracts "
+        + "and thus, we do not take any responsibilities concerning the answers and we "
+        + "clarify that the user of this survey needs to verify, and adapt their "
+        + "contracts accordingly, to comply with all national and international laws."
+    ),
+    "countries_first": [
+        "LU",
+        "BE",
+        "FR",
+        "DE",
+        "NL",
+        "GB",
+    ],
+    "defaultLanguage": "en",
+    "languages": [
+        ("en", "English"),
+        ("fr", "French"),
+        ("de", "German"),
+    ],
+    # Logos paths
+    "cases_logo": "templates/report/images/cases_logo.svg",
+    "secin_logo": "templates/report/images/secin_logo.svg",
+    # Minimal score
+    "minimal_acceptable_score": 65,
+    # Custom parts of templates
+    #   main dir for PARTS_TEMPLATE_DIR:
+    "templates_parts_dir": "templates/parts",
+    "templates_parts": {
+        # path of the templates parts
+        "terms": "terms_part.html",
+        "footer": "footer_part.html",
+        "main_logo": "logo_part.html",
+    },
+    # Available modules
+    "modules": {
+        "reportDownload": True,
+        "reportEmail": False,
+        "requestDiagnostic": True,
+        "requestTraining": True,
+        "displayResults": True,
+    },
+    # Available report parts
+    "report": {
+        "introduction": True,
+        "description": True,
+        "results": True,
+        "recommendations": True,
+        "questions": True,
+    },
+    "chart_exclude_sections": [],
+}
