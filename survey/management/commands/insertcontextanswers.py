21d679a2a5b07af6d4e42d5d81f7e70da0c3b2ec
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2022-01-20 16:55:39 +0100

    Added the data and implement the script to fill the context answers data.

--- survey/management/commands/insertcontextanswers.py
+++ survey/management/commands/insertcontextanswers.py
@@ -8,22 +8,54 @@ $ python manage.py insertcontextanswers
 
 import json
 from django.core.management.base import BaseCommand
-from survey.models import SurveyUserAnswer, SurveyQuestionAnswer
+from survey.models import SurveyUser, SurveyQuestion, SurveyUserAnswer, SurveyQuestionAnswer
 
 
 class Command(BaseCommand):
-    help = """Export a set of questions and answers.
-    Usage: ``python manage.py export_questions >> out.json``"""
+    help = """Creates context questions answers based on the old data structure."""
 
     def handle(self, *args, **options):
-        with open("./contrib/out.json") as f:
+        # Added the site-name as param:
+        site_name = 'fit4privacy'
+        with open("./contrib/" + site_name + "_users_data.json") as f:
             data = json.loads(f.read())
 
-        users = [elem for elem in data if elem["model"] == "survey.surveyuser"]
+        question_employees = SurveyQuestion.objects.get(
+            label="How many employees?", section__label="__context"
+        )
+        question_country = SurveyQuestion.objects.get(
+            label__contains="your country", section__label="__context"
+        )
+        question_sector = SurveyQuestion.objects.get(
+            label="What is your sector?", section__label="__context"
+        )
 
-        for user in users:
+        for user in data:
+            user = SurveyUser.objects.get(user_id=user["user_id"])
+            employees_answer = SurveyQuestionAnswer.objects.get(
+                question=question_employees, value=user["e_count"]
+            )
+            country_answer = SurveyQuestionAnswer.objects.get(
+                question=question_country, aindex=1
+            )
+            sector_answer = SurveyQuestionAnswer.objects.get(
+                question=question_sector, value=user["sector"]
+            )
 
-            print(user)
+            SurveyUserAnswer.objects.create(
+                user=user,
+                answer=employees_answer,
+                uvalue=user["e_count"],
+            )
 
-            SurveyQuestionAnswer()
-            SurveyUserAnswer()
+            SurveyUserAnswer.objects.create(
+                user=user,
+                answer=country_answer,
+                uvalue=user["country_code"],
+            )
+
+            SurveyUserAnswer.objects.create(
+                user=user,
+                answer=sector_answer,
+                uvalue=user["sector"],
+            )
