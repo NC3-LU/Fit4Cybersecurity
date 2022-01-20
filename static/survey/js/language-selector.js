066accaffa59e257198d2934282305bb0d99151a
jfrocha <juanferocha@hotmail.com>
2022-01-20 14:25:57 +0100

    Removed flags in language selector

--- static/survey/js/language-selector.js
+++ static/survey/js/language-selector.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    $('.flag-icon-en').addClass('flag-icon-gb').removeClass('flag-icon-en');
-    $('.flag-icon-lb').addClass('flag-icon-lu').removeClass('flag-icon-lb');
-
     let languageSelector = $("#language-selector a.dropdown-toggle").html();
     if (languageSelector) {
         var currentLanguageValue = languageSelector.replace(/\r?\n|\s/g, "");
