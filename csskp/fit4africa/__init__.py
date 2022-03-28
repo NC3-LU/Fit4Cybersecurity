7790159b1dcc40b1d588af4a6d1da2098a7aa503
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2022-03-28 15:40:55 +0200

    Added the config for Fit4Africa.

---
+++ csskp/fit4africa/__init__.py
@@ -0,0 +1,62 @@
+from django.utils.translation import gettext_lazy
+
+CUSTOM = {
+    # Generic configurations
+    "tool_name": "Cyber4Africa",
+    "intro_text": gettext_lazy(
+        "This survey will ask a few questions and provide "
+        + "recommendations. Keep in mind, that it is a self-assessment tool and that it "
+        + "only touches the surface of information security by giving a very basic maturity "
+        + "level estimate and some basic recommendations."
+    ),
+    "countries_first": [],
+    "defaultLanguage": "en",
+    "languages": [
+        ("en", "English"),
+    ],
+    # Logos paths
+    "tool_logo": "static/images/logo-fit4operatorsurvey.png",
+    "cases_logo": "templates/report/images/cases_logo.svg",
+    "secin_logo": "templates/report/images/secin_logo.svg",
+    # Minimal score
+    "minimal_acceptable_score": 65,
+    # Show or not the questions number left to complete the survey.
+    "show_progress_questions_numbers": True,
+    # Custom parts of templates
+    #   main dir for PARTS_TEMPLATE_DIR:
+    "templates_parts_dir": "templates/parts",
+    "templates_parts": {
+        # path of the templates parts
+        "terms": "terms_part.html",
+        "footer": "footer_part.html",
+        "main_logo": "logo_part.html",
+    },
+    # Available modules
+    "modules": {
+        "reportDownload": True,
+        "reportEmail": False,
+        "requestDiagnostic": False,
+        "requestTraining": False,
+        "displayResults": False,
+    },
+    # Available report parts
+    "report": {
+        "introduction": True,
+        "description": False,
+        "results": False,
+        "recommendations": True,
+        "questions": True,
+    },
+    # Available stats charts
+    "stats": {
+        "activity": True,
+        "sector": True,
+        "size": True,
+        "country": True,
+        "status": True,
+        "language": False,
+        "section": False,
+        "category": False,
+    },
+    "chart_exclude_sections": [],
+}
