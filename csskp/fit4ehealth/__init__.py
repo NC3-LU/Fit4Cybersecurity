bafdd5819f35858cd987f0caf8b8d85db623b05e
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2022-04-11 15:43:51 +0200

    Checnged the configuration of fit4ehelath tool, added some custom templates.

--- csskp/fit4ehealth/__init__.py
+++ csskp/fit4ehealth/__init__.py
@@ -3,7 +3,19 @@ from django.utils.translation import gettext_lazy
 CUSTOM = {
     # Generic configurations
     "tool_name": "Fit4eHealth",
-    "intro_text": gettext_lazy("Description of the tool."),
+    "intro_text": gettext_lazy(
+        "The objective of the self-assessment tool, is to measure the maturity of an organization "
+        "in terms of applicable good practices in the field of information security.<br />"
+        "This document, as the outcome of the self-assessment, is for the exclusive use of "
+        "the user. It is in this respect confidential.<br />Given the methodology used and "
+        "the fact that it is a self-assessment, the overall results cannot be exhaustive in any "
+        "way.<br />As such, the actual risk assessment or the list of identified risks and "
+        "vulnerabilities is therefore based on the information provided by the user.<br /><br />"
+        "The tool may provide recommendations, but the user must be aware that these are neither "
+        "exclusive nor exhaustive.<br />It should also be noted that the information you have "
+        "provided to us is recorded for statistical reasons.<br />Due to the nature of the data, "
+        "we cannot identify you, the self-assessment is performed anonymously."
+    ),
     "countries_first": [],
     "defaultLanguage": "en",
     "languages": [
@@ -17,7 +29,7 @@ CUSTOM = {
     # Custom CSS:
     "survey_css": "survey/css/style.css",
     # Minimal score
-    "minimal_acceptable_score": 65,
+    "minimal_acceptable_score": 75,
     # Show or not the questions number left to complete the survey.
     "show_progress_questions_numbers": True,
     # Custom parts of templates
@@ -33,7 +45,7 @@ CUSTOM = {
     "modules": {
         "reportDownload": True,
         "reportEmail": False,
-        "requestDiagnostic": False,
+        "requestDiagnostic": True,
         "requestTraining": False,
         "displayResults": True,
     },
