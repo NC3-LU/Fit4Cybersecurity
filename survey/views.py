7d86f28642b95a203e86de181e34896c19c2efeb
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2022-06-14 15:42:26 +0200

    Format for black.

--- survey/views.py
+++ survey/views.py
@@ -118,7 +118,9 @@ def change_lang(request, lang: str):
     user.save()
 
     if (
-        (user.is_survey_in_progress() or user.is_survey_under_review())
+        (
+            user.is_survey_in_progress() or user.is_survey_under_review()
+        )
         and previous_path.__contains__("/survey/question/")
     ):
         return HttpResponseRedirect(previous_path)
