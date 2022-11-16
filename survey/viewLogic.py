7aa253c8a75cbdc055a3a3a9021dc1e9dd9ebf25
jfrocha <juanferocha@hotmail.com>
2022-11-16 16:40:18 +0100

    Fixed count tottal of questions when is a simple questionnaire tree

--- survey/viewLogic.py
+++ survey/viewLogic.py
@@ -813,13 +813,15 @@ def increment_questions_sequence_order_from(
 
 
 def get_total_questions_number(user: SurveyUser, question_index: int) -> int:
-    if (
-        not SurveyAnswerQuestionMap.objects.exists()
-        or CUSTOM["is_simple_questionnaire_tree"]
-    ):
+    if not SurveyAnswerQuestionMap.objects.exists():
         return SurveyUserQuestionSequence.objects.filter(
             user=user, is_active=True
         ).count()
+
+    if CUSTOM["is_simple_questionnaire_tree"]:
+        return SurveyAnswerQuestionMap.objects.filter(
+            branch=get_last_user_sequence(user).branch
+        ).order_by("question").distinct("question").count() + 1
 
     branches_number = SurveyAnswerQuestionMap.objects.aggregate(Max("branch"))[
         "branch__max"
