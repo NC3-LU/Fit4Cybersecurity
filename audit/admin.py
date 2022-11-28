40be451c4a3923aa00c0ee1bb15846f94e2e7c2d
Juan Rocha <juan.rocha@securitymadein.lu>
2022-11-28 16:50:01 +0100

    [fit4CSA] Improved audit models

--- audit/admin.py
+++ audit/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
 
-from audit.models import AuditCompany
-from audit.models import CompanyUser
-from audit.models import Auditor
+from audit.models import Company
+from audit.models import AuditUser
 from audit.models import Audit
-from audit.models import AuditAuditor
 from audit.models import AuditCompanyUser
 
-admin.site.register(AuditCompany)
-admin.site.register(CompanyUser)
-admin.site.register(Auditor)
+admin.site.register(Company)
+admin.site.register(AuditUser)
 admin.site.register(Audit)
-admin.site.register(AuditAuditor)
 admin.site.register(AuditCompanyUser)
--- audit/models.py
+++ audit/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from survey.models import SurveyUser
-
+from django.contrib.auth.models import User
 
 class Company(models.Model):
     name = models.TextField()
@@ -9,25 +9,9 @@ class Company(models.Model):
     admin_last_name = models.TextField()
 
 
-class AuditCompany(models.Model):
-    name = models.TextField()
-    address = models.TextField()
-    contact_email = models.EmailField()
-    contact_phone = models.CharField(max_length=12)
-
-
-class CompanyUser(models.Model):
-    first_name = models.TextField()
-    last_name =  models.TextField()
-    email = models.EmailField()
+class AuditUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
-
-
-class Auditor(models.Model):
-    first_name = models.TextField()
-    last_name =  models.TextField()
-    email = models.EmailField()
-    company = models.ForeignKey(AuditCompany, on_delete=models.CASCADE)
 
 
 class Audit(models.Model):
@@ -38,11 +22,6 @@ class Audit(models.Model):
     certificate_revocation_date = models.DateField()
 
 
-class AuditAuditor(models.Model):
-    audit = models.ForeignKey(Audit, on_delete=models.CASCADE)
-    auditor = models.ForeignKey(Auditor, on_delete=models.CASCADE)
-
-
 class AuditCompanyUser(models.Model):
     audit = models.ForeignKey(Audit, on_delete=models.CASCADE)
-    company_user = models.ForeignKey(CompanyUser, on_delete=models.CASCADE)
+    company_user = models.ForeignKey(AuditUser, on_delete=models.CASCADE)
