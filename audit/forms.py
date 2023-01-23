55f58038f2d42c762ee2685eeb6658dd277fd9e3
Juan Rocha <juan.rocha@securitymadein.lu>
2023-01-23 16:00:16 +0100

    [Fit4CSA] Added forms

--- audit/forms.py
+++ audit/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from audit.globals import QUESTION_STATUS
 
 
 class SignUpForm(UserCreationForm):
@@ -19,3 +20,68 @@ class SignUpForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+
+class ReferencesTextarea(forms.Form):
+    def __init__(self, *args, **kwargs):
+        id_audit_question = kwargs.pop("id")
+        reference = kwargs.pop("reference")
+        type_of_company = kwargs.pop("type_of_company")
+        super().__init__(*args, **kwargs)
+
+        self.fields["references"] = forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    "id": id_audit_question,
+                    "rows": 6,
+                    "class": "w-100 h-100 border-0 bg-transparent",
+                    "placeholder": "",
+                    "onblur": "onBlurTextarea(this)",
+                }
+            ),
+            disabled=True if type_of_company == "AD" else False,
+            initial=reference,
+        )
+
+
+class ObservationsTextarea(forms.Form):
+    def __init__(self, *args, **kwargs):
+        id_audit_question = kwargs.pop("id")
+        observation = kwargs.pop("observation")
+        type_of_company = kwargs.pop("type_of_company")
+        super().__init__(*args, **kwargs)
+
+        self.fields["observations"] = forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    "id": id_audit_question,
+                    "rows": 6,
+                    "class": "w-100 h-100 border-0 bg-transparent",
+                    "placeholder": "",
+                    "onblur": "onBlurTextarea(this)",
+                }
+            ),
+            disabled=True if type_of_company == "CS" else False,
+            initial=observation,
+        )
+
+
+class StatusChoices(forms.Form):
+    def __init__(self, *args, **kwargs):
+        id_audit_question = kwargs.pop("id")
+        status = kwargs.pop("status")
+        type_of_company = kwargs.pop("type_of_company")
+        super().__init__(*args, **kwargs)
+
+        self.fields["status"] = forms.ChoiceField(
+            choices=QUESTION_STATUS,
+            widget=forms.Select(
+                attrs={
+                    "id": id_audit_question,
+                    "class": "w-100 border-0 p-0 bg-transparent",
+                    "onchange": "onChangeSelect(this)",
+                }
+            ),
+            disabled=True if type_of_company == "CS" else False,
+            initial=status,
+        )
