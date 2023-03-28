8a6e635c0c7bd3ee82f863c954af5aecc86b2e52
Juan Rocha <juan.rocha@nc3.lu>
2023-03-28 09:53:24 +0200

    [fit4csa]pre-commit fixes

--- csskp/fit4csa/__init__.py
+++ csskp/fit4csa/__init__.py
@@ -4,18 +4,35 @@ CUSTOM = {
     # Generic configurations
     "tool_name": "Fit4CSA",
     "intro_text": gettext_lazy(
-        'Fit4CSA is a self-assessment tool to streamline the process of applying for a basic-level cybersecurity certification in the context of the CyberSecurity Act (CSA - EU 2019/881). Fit4CSA is part of the <a href="https://coral-project.org/" target="_blank">CORAL</a> EU-funded project.'
+        "Fit4CSA is a self-assessment tool to streamline the process of applying for a basic-level "
+        "cybersecurity certification in the context of the CyberSecurity Act (CSA - EU 2019/881). "
+        'Fit4CSA is part of the <a href="https://coral-project.org/" target="_blank">CORAL</a> '
+        "EU-funded project."
         "<br><br>"
         "<h4>How does Fit4CSA work?</h4>"
         "<ol>"
-        "<li>Select what you would like to assess: an ICT service, an ICT process, an ICT product - Web application or an ICT generic product.</li>"
-        "<li>Depending on your choice, you will need to fill in a questionnaire with both single choice and multiple choice questions. If you have supporting evidence for each of your answers (a policy, procedure, etc.), we recommend that you to keep track of this all throughout the questionnaire in order to establish a mapping of this evidence as you progress.</li>"
-        "<li>At the end of the survey, you will be given a score and a set of recommendations. Your cybersecurity maturity can improve if you follow these recommendations.</li>"
-        "<li>If you scored at least 85%, Fit4CSA will additionally ask if you want a CSA conformity self-assessment, or to apply a basic-level certification. In the first case, you will be able to download the report with your answers and use it as a basis of your conformity self-assessment. In the second case, you will be asked to register and start an audit process based on the report issued within Fit4CSA. Keep in mind that all evidence supporting your answers might be requested later by the auditor of your choice.</li>"
+        "<li>Select what you would like to assess: an ICT service, an ICT process, "
+        "an ICT product - Web application or an ICT generic product.</li>"
+        "<li>Depending on your choice, you will need to fill in a questionnaire with both single "
+        "choice and multiple choice questions. If you have supporting evidence for each of your "
+        "answers (a policy, procedure, etc.), we recommend that you to keep track of this all "
+        "throughout the questionnaire in order to establish a mapping of this evidence as you "
+        "progress.</li>"
+        "<li>At the end of the survey, you will be given a score and a set of recommendations. "
+        "Your Scybersecurity maturity can improve if you follow these recommendations.</li>"
+        "<li>If you scored at least 85%, Fit4CSA will additionally ask if you want a CSA "
+        "conformity self-assessment, or to apply a basic-level certification. In the first case, "
+        "you will be able to download the report with your answers and use it as a basis of your "
+        "conformity self-assessment. In the second case, you will be asked to register and start "
+        "an audit process based on the report issued within Fit4CSA. Keep in mind that all "
+        "evidence supporting your answers might be requested later by the auditor of your choice."
+        "</li>"
         "</ol>"
         "The first 3 steps of this process are anonymous."
         "<br><br>"
-        "If at some point, you wish to continue the survey later, you click on the <strong>Continue later button</strong> and save the provided link separately. Using that link, you can pick up where you left off anytime."
+        "If at some point, you wish to continue the survey later, you click on the "
+        "<strong>Continue later button</strong> and save the provided link separately. "
+        "Using that link, you can pick up where you left off anytime."
     ),
     "countries_first": [
         "LU",
