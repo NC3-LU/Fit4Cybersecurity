ca9add16f01520dbd2d68660bcce05a3e878b8e4
Juan Rocha <juan.rocha@nc3.lu>
2023-06-06 09:24:34 +0200

    [surveyAlto]Changed intro survey

--- survey/urls.py
+++ survey/urls.py
@@ -6,15 +6,15 @@ from survey import views
 
 urlpatterns = [
     path("", views.index),
-    path("start", views.start),
-    path("question/<int:question_index>", views.handle_question_form),
+    # path("start", views.start),
+    # path("question/<int:question_index>", views.handle_question_form),
     path("language/<slug:lang>", views.change_lang),
-    path("resume/", views.resume),
-    path("review", views.review),
-    path("finish", views.finish),
-    path("report", views.show_report),
-    path("feedback", views.save_general_feedback),
-    path("companies", views.get_companies),
+    # path("resume/", views.resume),
+    # path("review", views.review),
+    # path("finish", views.finish),
+    # path("report", views.show_report),
+    # path("feedback", views.save_general_feedback),
+    # path("companies", views.get_companies),
     path("api/", include("survey.api.urls")),
 ]
 if DEBUG:
