187ad4c8637b46177bcb53601d91d5932b90e4f6
Juan Rocha <juan.rocha@nc3.lu>
2024-08-06 09:36:21 +0200

    [usrveyBelgium]Initizialized survey files

---
+++ csskp/surveylouvain/__init__.py
@@ -0,0 +1,67 @@
+from django.utils.translation import gettext_lazy
+
+CUSTOM = {
+    # Generic configurations
+    "tool_name": "Survey on visiting Louvain",
+    "intro_text": gettext_lazy("[INTRO survey on visiting Louvain]"),
+    "countries_first": [
+        "LU",
+        "BE",
+        "FR",
+        "DE",
+        "NL",
+        "GB",
+    ],
+    "defaultLanguage": "en",
+    "languages": [
+        ("en", "English"),
+    ],
+    # Logos paths
+    "right_cover_logo": "templates/report/images/nc3_logo.svg",
+    "left_cover_logo": "templates/report/images/lhc_logo.svg",
+    # Custom CSS:
+    "survey_css": "survey/css/style.css",
+    # Minimal score
+    "minimal_acceptable_score": 65,
+    # Show or not the questions number left to complete the survey.
+    "show_progress_questions_numbers": True,
+    # Custom parts of templates
+    #   main dir for PARTS_TEMPLATE_DIR:
+    "templates_parts_dir": "templates/parts",
+    "templates_parts": {
+        # path of the templates parts
+        "terms": "terms_part.html",
+        "footer": "footer_part.html",
+        "main_logo": "logo_part.html",
+        "privacy": "privacy_policy_part.html",
+    },
+    # Available modules
+    "modules": {
+        "reportDownload": False,
+        "reportEmail": False,
+        "requestDiagnostic": False,
+        "requestTraining": False,
+        "displayResults": False,
+    },
+    # Available report parts
+    "report": {
+        "introduction": True,
+        "description": True,
+        "results": True,
+        "recommendations": True,
+        "questions": True,
+    },
+    # Available stats charts
+    "stats": {
+        "activity": False,
+        "sector": False,
+        "size": False,
+        "country": False,
+        "status": True,
+        "language": False,
+        "section": False,
+        "category": False,
+        "current_question": True,
+    },
+    "chart_exclude_sections": [],
+}
